f00d36195a9c1ce6b2d59e3037b66a47a5255123
faizabatta1 <faizabatta1@github.com>
2023-08-12 03:30:37 +0300

    x

--- controllers/driverController.js
+++ controllers/driverController.js
@@ -158,79 +158,83 @@ const puppeteer = require('puppeteer')
 // }
 
 const createNewDriver = async (req,res) =>{
-    console.log(req.headers)
-    console.log(req.body)
-    const { data, token } = req.headers
-    const information = JSON.parse(data)
+    try{
+        console.log(req.headers)
+        console.log(req.body)
+        const { data, token } = req.headers
+        const information = JSON.parse(data)
 
-    console.log(information)
+        console.log(information)
 
-    let values = Object.values(req.body).map(e => JSON.parse(e))
+        let values = Object.values(req.body).map(e => JSON.parse(e))
 
-    const groupedData = values.reduce((acc, obj) => {
-        if (!acc[obj.form]) {
-            acc[obj.form] = [];
-        }
-
-        acc[obj.form].push(obj);
-        return acc;
-    }, {});
-
-
-    const browser = await puppeteer.launch({
-        headless: 'new'
-    });
-    const page = await browser.newPage();
-
-    // Load the HTML template
-    const htmlTemplate = fs.readFileSync('templates/driver.html', 'utf8');
-    let decodedToken = jwt.verify(token,'your-secret-key')
-    let user = await User.findOne({ _id: decodedToken.userId })
-
-    // Replace placeholders with dynamic data
-    const template_data = {
-        location: information.locations,
-        car: information.boardNumber + "  " + information.privateNumber,
-        shift: information.day + " - " + information.period,
-        violations: information.trafficViolations,
-        date: information.date,
-        user: user.name + ' - ' + user.accountId,
-        rows: [...groupedData['First'],...groupedData['Second']].map(e => {
-            return {
-                title: e.title,
-                status: e.value != 'Nei' ? 'Ja' : 'Nei',
-                notes: e.value != "Nei" ? (e.hasRequiredDescription ? e.value : 'XXX') : 'XXX'
-            };
-        }),
-        images: req.files.map(file =>{
-            console.log(`${file.fieldname} - ${process.env.BASE_URL + file.path.split('public')[1].replaceAll('\\','/')}`)
-
-            return {
-                fieldname: file.fieldname,
-                path: process.env.BASE_URL + file.path.split('public')[1].replaceAll('\\','/')
+        const groupedData = values.reduce((acc, obj) => {
+            if (!acc[obj.form]) {
+                acc[obj.form] = [];
             }
-        }),
-    };
-    const filledTemplate = Handlebars.compile(htmlTemplate)(template_data);
 
-    let filename = `driver_${Date.now()}.pdf`
-
-    // Generate PDF from filled template
-    await page.setContent(filledTemplate);
-    await page.pdf({ path: `./public/profiles/${filename}`, format: 'A4' });
-
-    let pdf = new PDF({
-        name: filename,
-        link: process.env.BASE_URL + 'profiles/' + filename,
-        userId: decodedToken.userId
-    })
-
-    await pdf.save()
-    console.log(`PDF saved: ${process.env.BASE_URL + 'profiles/' + filename}`);
+            acc[obj.form].push(obj);
+            return acc;
+        }, {});
 
 
-    await browser.close();
-    res.status(200).json({ message: 'PDF generated and saved successfully' });
+        const browser = await puppeteer.launch({
+            headless: 'new'
+        });
+        const page = await browser.newPage();
+
+        // Load the HTML template
+        const htmlTemplate = fs.readFileSync('templates/driver.html', 'utf8');
+        let decodedToken = jwt.verify(token,'your-secret-key')
+        let user = await User.findOne({ _id: decodedToken.userId })
+
+        // Replace placeholders with dynamic data
+        const template_data = {
+            location: information.locations,
+            car: information.boardNumber + "  " + information.privateNumber,
+            shift: information.day + " - " + information.period,
+            violations: information.trafficViolations,
+            date: information.date,
+            user: user.name + ' - ' + user.accountId,
+            rows: [...groupedData['First'],...groupedData['Second']].map(e => {
+                return {
+                    title: e.title,
+                    status: e.value != 'Nei' ? 'Ja' : 'Nei',
+                    notes: e.value != "Nei" ? (e.hasRequiredDescription ? e.value : 'XXX') : 'XXX'
+                };
+            }),
+            images: req.files.map(file =>{
+                console.log(`${file.fieldname} - ${process.env.BASE_URL + file.path.split('public')[1].replaceAll('\\','/')}`)
+
+                return {
+                    fieldname: file.fieldname,
+                    path: process.env.BASE_URL + file.path.split('public')[1].replaceAll('\\','/')
+                }
+            }),
+        };
+        const filledTemplate = Handlebars.compile(htmlTemplate)(template_data);
+
+        let filename = `driver_${Date.now()}.pdf`
+
+        // Generate PDF from filled template
+        await page.setContent(filledTemplate);
+        await page.pdf({ path: `./public/profiles/${filename}`, format: 'A4' });
+
+        let pdf = new PDF({
+            name: filename,
+            link: process.env.BASE_URL + 'profiles/' + filename,
+            userId: decodedToken.userId
+        })
+
+        await pdf.save()
+        console.log(`PDF saved: ${process.env.BASE_URL + 'profiles/' + filename}`);
+
+
+        await browser.close();
+        res.status(200).json({ message: 'PDF generated and saved successfully' });
+    }catch (error){
+        req.status(500).send(error.message)
+    }
 }
 
 const getAllDrivers = async (req,res) =>{
